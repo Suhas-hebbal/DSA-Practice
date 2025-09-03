3d13e70723e79532122607baa1716ba50ee97b83
Suhas-hebbal <154068326+Suhas-hebbal@users.noreply.github.com>
2025-09-03 20:06:28 +0530

    Create countDigitNum.java

---
+++ countDigitNum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countDigit(int n) {
+        int count=0;
+        if(n==0){
+            count++;
+        }
+
+        while(n>0){
+            int rem=n%10;
+            n=n/10;
+            count++;
+        }
+
+        return count;
+    }
+}
